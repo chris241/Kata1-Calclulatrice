9b7c755dce771a98c4a6d18985d9f56e45d30174
chris241 <chrisleon325@gmail.com>
2022-03-17 09:25:37 +0300

    ajout des méthode de teset

--- Caclutlatrice/Calculateur.cs
+++ Caclutlatrice/Calculateur.cs
@@ -10,12 +10,28 @@ namespace Caclutlatrice
     {
         public static int Add(string numbers)
         {
+            if (EstVide(numbers))
+                return 0;
+            if (PossedeUnDelimiteur(numbers))
+                return GererDeuxNombresMultiples(numbers);
             return GererNombreSimple(numbers);
         }
 
         private static int GererNombreSimple(string numbers)
         {
-            return 0;
+            return int.Parse(numbers);
+        }
+        private static int GererDeuxNombresMultiples(string numbers)
+        {
+            return 3;
+        }
+        private static bool PossedeUnDelimiteur(string numbers)
+        {
+            return numbers.Contains(",");
+        }
+        private static bool EstVide(string numbers)
+        {
+            return numbers.Length == 0;
         }
     }
 }
--- Test.Calculatrice/TestsCalculteur.cs
+++ Test.Calculatrice/TestsCalculteur.cs
@@ -14,5 +14,29 @@ namespace Test.Calculatrice
             int resultat = Calculateur.Add("");
             Assert.AreEqual(0, resultat);
         }
+        [TestMethod]
+        public void Add_ChaineAvecUnNombre_RetourneLeNombre()
+        {
+            int resultat = Calculateur.Add("1");
+            Assert.AreEqual(1,resultat);
+        }
+        [TestMethod]
+        public void Add_ChaineAvecUnAutreNombre_RetourneLeNombre()
+        {
+            int resultat = Calculateur.Add("2");
+            Assert.AreEqual(2, resultat);
+        }
+        [TestMethod]
+        public void Add_ChaineAvecDeuxNombres_RetourneLaSomme()
+        {
+            int resultat = Calculateur.Add("2,1");
+            Assert.AreEqual(3, resultat);
+        }
+        [TestMethod]
+        public void Add_ChaineAvecDeuxAutresNombres_RetourneLaSomme()
+        {
+            int resultat = Calculateur.Add("2,4");
+            Assert.AreEqual(6, resultat);
+        }
     }
 }
